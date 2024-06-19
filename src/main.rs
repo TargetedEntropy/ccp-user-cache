e71b07ab9e4618fb51ea58bec0b79bacbb58c593
TargetedEntropy <merriam@gmail.com>
2024-06-19 14:21:45 +0000

    renames, added scopes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 mod config;
 mod db;
+mod dtos;
+mod error;
+mod extractors;
+mod models;
+mod scopes;
+mod utils;
 
-use actix_web::{get, middleware::Logger, web, App, HttpResponse, HttpServer, Responder};
+use actix_cors::Cors;
+use actix_web::{
+    get, http::header, middleware::Logger, web, App, HttpResponse, HttpServer, Responder,
+};
 use config::Config;
 use db::DBClient;
 use dotenv::dotenv;
@@ -16,6 +25,7 @@ pub struct AppState {
 // OMG WE HAVE A MAIN
 #[actix_web::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
     // Check for ENV Logging
     if std::env::var_os("RUST_LOG").is_none() {
         std::env::set_var("RUST_LOG", "actix_web=info");
@@ -33,7 +43,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .max_connections(10)
         .connect(&config.database_url)
         .await?;
+    
+    // Migrations
+    match sqlx::migrate!("./migrations").run(&pool).await {
+        Ok(_) => println!("Migrations executed successfully."),
+        Err(e) => eprintln!("Error executing migrations: {}", e),
+    };
 
+    // Create the client
     let db_client = DBClient::new(pool);
 
     // Start the AppState
@@ -48,20 +65,33 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         format!("Server is running on http://localhost:{}", config.port)
     );
 
-    // START THE SERVER GROG
+    // HTTP Server with ALLOWED ORIGINS, #fancy
     HttpServer::new(move || {
+        let cors = Cors::default()
+            .allowed_origin("http://localhost:3000")
+            .allowed_origin("http://localhost:8000")
+            .allowed_methods(vec!["GET", "POST"])
+            .allowed_headers(vec![
+                header::CONTENT_TYPE,
+                header::AUTHORIZATION,
+                header::ACCEPT,
+            ])
+            .supports_credentials();
+
         App::new()
             .app_data(web::Data::new(app_state.clone()))
+            .wrap(cors)
             .wrap(Logger::default())
+            .service(scopes::auth::auth_scope())
+            .service(scopes::users::users_scope())
             .service(health_checker_handler)
     })
-    .bind(("0.0.0.0", config.port))?
+    .bind(("0.0.0.0", config.port))? // why are we binding to * when we only allow localhost, cause yee haw thats why.
     .run()
     .await?;
 
     Ok(())
 }
-
 
 // AM I A REAL API
 #[get("/api/healthchecker")]
